5f9c7910ba74667e6f451db7966cc46bc871f7d4
Joan Lajarin <joanlajarinreinares@gmail.com>
2024-02-26 17:24:18 +0100

    html with tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ function App() {
 
   const [questions, setQuestions ] = useState([
     'Which country is Kuala Lumpur the capital ?',
+    'Which country does this flag ${flag} belong to?',
     'Which country is Kuala Lumpur the capital ?',
     'Which capital is "Country" the country ?',
     'Which capital is "Country" the country ?',
@@ -16,36 +17,39 @@ function App() {
     'Which currency is "Country" the country?',
     'Which currency is "Country" the country?',
     'Which subregions is "Country" the country?',
-    'Which subregions is "Country" the country?'
   ])
 
   useEffect(() => {
     //fetch data api
   },[])
   return (
-      <section className="bg-[#393F6E]">
-        <h3 className='text-[#8B8EAB] text-[14px] font-semibold'>Country Quiz</h3>
-        <section className='flex'>
-
+    <div className='h-full w-full flex flex-col justify-center items-center'>
+      <section className="bg-[#393F6E] max-w-[820px] pt-[36px] pb-[62px] px-[140px] rounded-xl flex flex-col items-center">
+        <h3 className='text-[#8B8EAB] text-[14px] font-semibold mb-[16px]'>Country Quiz</h3>
+        <section className='flex mb-[32px]'>
+        <div className='bg-gradient-to-r from-[#E65895] to-[#BC6BE8] size-[40px] rounded-full flex justify-center items-center'>
+          <span>1</span>
+        </div>
         </section>
-        <section className='flex flex-col'>
-          <h1>Which country is Kuala Lumpur the capital?</h1>
-        <div className='grid grid-cols-2'> 
-          <div className='bg-[#343964] rounded-md'>
-            <span>Sweden</span>
+        <section className='flex flex-col items-center'>
+          <h1 className='text-[20px] mb-[40px]'>Which country is Kuala Lumpur the capital?</h1>
+        <div className='grid grid-cols-2 gap-[24px] w-[450px]'> 
+          <div className='bg-[#343964] rounded-xl py-[15px] px-[35px] flex justify-center'>
+            <span className='text-[16px] font-semibold'>Sweden</span>
           </div>
-          <div className='bg-[#343964] rounded-md'>
-            <span>Vietnam</span>
+          <div className='bg-gradient-to-r from-[#E65895] to-[#BC6BE8] rounded-xl py-[15px] px-[35px] flex justify-center'>
+            <span className='text-[16px] font-semibold'>Vietnam</span>
           </div>          
-          <div className='bg-[#343964] rounded-md'>
-            <span>Malasya</span>
+          <div className='bg-[#343964] rounded-xl py-[15px] px-[35px] flex justify-center'>
+            <span className='text-[16px] font-semibold'>Malasya</span>
           </div>          
-          <div className='bg-[#343964] rounded-md'>
-            <span>Austria</span>
+          <div className='bg-[#343964] rounded-xl py-[15px] px-[35px] flex justify-center'>
+            <span className='text-[16px] font-semibold'>Austria</span>
           </div>
         </div>
         </section>
       </section>
+    </div>
   )
 }
 
